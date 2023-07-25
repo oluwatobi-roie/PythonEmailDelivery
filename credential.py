863e050354f9efcf3363c44dc711883efab0fdac
Oluwatobi Victor <68193330+oluwatobi-roie@users.noreply.github.com>
2023-07-25 20:49:17 +0100

    Update Credetial.py

---
+++ credential.py
@@ -0,0 +1,14 @@
+# Gmail Variables
+Credential_Gmail = 'your_gmail_address'
+Credential_Gmail_password = 'your_gmail_password'
+Credential_gmail_Server = 'smtp.gmail.com'
+
+# General Variables
+Credential_Bcc = 'additional_Bcc_email' #Not used in code
+Credential_Port = '587'
+
+# DB credential Definition
+Credential_IP = "ipAddress_where_your_sql is located" #use localhost if sql is on the same computer you are working from
+Credential_TraccarUser = "SQL_UserName"
+Credential_TraccarPass = "SQL_Password"
+Credential_TraccarDB = "SQL_DatabaseName"
